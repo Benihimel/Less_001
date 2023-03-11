1b9eee8ba80b4fa9af5505013b759b66635c2f17
Benihimel <127148808+Benihimel@users.noreply.github.com>
2023-03-11 17:41:01 -0500

    Add task03

---
+++ task003.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int C = number % 2; 
+if (C == 0)
+{
+    Console.WriteLine("This is an even number");
+}
+else
+{
+    Console.WriteLine("This is an odd number");
+}
+
